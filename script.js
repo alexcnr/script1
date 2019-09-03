b8df022dac717922f294bd415e77f6c88cee65c8
alexcnr <54246135+alexcnr@users.noreply.github.com>
2019-09-03 15:28:35 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+var foodInGrams = 100;
+var sleepInHours = 15;
+var eatevery10 = foodInGrams / 10;
+var jumps = eatevery10 * 3;
+console.log(jumps);
+var pulls = sleepInHours * 2;
+console.log(pulls);
+var runs = (jumps * 1) + (pulls / 2);
+console.log(runs);
